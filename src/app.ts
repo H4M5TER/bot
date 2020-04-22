975364d0c66edd4b8b15a55844d09477f763a0c5
H4M5TER <zhuxx2003@gmail.com>
2020-04-22 22:36:20 +0800

    删除待办

--- src/app.ts
+++ src/app.ts
@@ -40,10 +40,13 @@ app.command("添加待办 <待办事项...>")
     .action(async ({ meta }, _message) => {
         let collection = db.collection("task")
         let tasks = await collection.findOne({ group: meta.groupId, user: meta.userId })
-        if (null == tasks)
+        if (null === tasks)
             collection.insertOne({ group: meta.groupId, user: meta.userId, count: 0 })
         let count = <number>tasks.count + 1
-        collection.updateOne({ group: meta.groupId, user: meta.userId }, {
+        collection.updateOne({
+            group: meta.groupId,
+            user: meta.userId
+        }, {
             $push: {
                 task: {
                     id: count,
@@ -53,6 +56,82 @@ app.command("添加待办 <待办事项...>")
             $set: { count: count }
         }).catch(err => console.log(err))
     })
+
+app.command("删除待办 <待办ID>")
+    .action(({ meta }, id) => {
+        let collection = db.collection("task");
+        collection.aggregate([{
+            $match: {
+                group: meta.groupId,
+                user: meta.userId
+            }
+        }, {
+            $project: {
+                _id: false,
+                task: true
+            }
+        }, {
+            $unwind: "$task"
+        }, {
+            $match: {
+                "task.id": {
+                    $ne: id
+                }
+            }
+        }]).toArray().then(tasks => {
+            collection.updateOne({
+                group: meta.groupId,
+                user: meta.userId
+            }, {
+                $unset: {
+                    task: ""
+                }
+            })
+            tasks.forEach(({ task }) => {
+                if (id < task.id)
+                    task.id -= 1
+                collection.update({
+                    group: null,
+                    user: 860844980
+                }, {
+                    $push: {
+                        task: task
+                    }
+                })
+            })
+        }).catch(e => console.error(e))
+    })
+
+// app.command("我的待办").action(async ({ meta }) => {
+//     db.collection("task").aggregate([{
+//         $match: {
+//             group: meta.groupId,
+//             user: meta.userId
+//         }
+//     }, {
+//         $project: {
+//             _id: false,
+//             task: true
+//         }
+//     }, {
+//         $unwind: "$task"
+//     }, {
+//         $sort: {
+//             "task.id": 1
+//         }
+//     }]).toArray().then(array => {
+//         array.forEach(task => { console.log(task) })
+//     }).catch(e => console.error(e))
+//     let task = (await db.collection("task")
+//         .findOne({ group: meta.groupId, user: meta.userId })).task
+//     task.forEach(task => {
+//         if ('group' === meta.messageType) {
+//             app.sender.sendGroupMsg(meta.groupId, task.)
+//         } else if ('private' === meta.messageType) {
+
+//         }
+//     })
+// })
 
 app.start()
     .catch((err: ExceptionInformation) => { console.error(err) })
