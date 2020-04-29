7df7bd96edf55d279296cf7b765319d138b2161a
H4M5TER <zhuxx2003@gmail.com>
2020-04-29 22:31:13 +0800

    format better

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,9 @@ import { appendFile } from "fs"
 
 app.receiver.on("message", async (meta) => {
     try {
-        console.log(`${(new Date(meta.time! * 1000)).toLocaleTimeString()}${(await app.sender.getGroupInfo(meta.groupId!)).groupName}(${meta.groupId})\n\t${meta.sender?.nickname}(${meta.userId}):${meta.message}`)
+        console.log(
+            `${(new Date(meta.time! * 1000)).toLocaleTimeString()}${(await app.sender.getGroupInfo(meta.groupId!)).groupName}(${meta.groupId})`
+            + `\n\t${meta.sender?.nickname}(${meta.userId}):${meta.message}`)
     } catch (e) {
         console.error(e)
     }
@@ -32,23 +34,28 @@ let room = new LiveTCP(user.room_id)
 room.on("live", () => {
     console.log("直播间已连接")
 })
-room.on("msg", (data) => {
+room.on("msg", async (data) => {
     try {
         if (data.cmd === "DANMU_MSG") {
             let date = new Date()
-            appendFile(`danmaku_${date.toLocaleDateString()}.log`, `[${date.toLocaleTimeString()}]${data.info[2][1]}(${data.info[2][0]}):${data.info[1]}`, () => { })
+            appendFile(`danmaku_${date.toLocaleDateString()}.log`,
+                `[${date.toLocaleTimeString()}]${data.info[2][1]}(${data.info[2][0]}):${data.info[1]}\n`,
+                () => { })
         }
         if (data.cmd === "LIVE") {
-            groups.forEach(async group_id => {
-                // TODO: 随机发表情包
-                app.sender.sendGroupMsg(group_id, `${user.nickname}开播了[CQ:at,qq=all]\n${(await axios.request<RoomInfo>(live_request_config)).data.data.room_info.title}\n${room_address}`)
-            })
+            for (let group_id of groups)
+                app.sender.sendGroupMsg(group_id,
+                    `${user.nickname}开播了[CQ:at,qq=all]`
+                    + `\n${(await axios.request<RoomInfo>(live_request_config)).data.data.room_info.title}`
+                    + `\n${room_address}`)
             app.sender.setGroupWholeBan(743492765, true)
         }
         if (data.cmd === "PREPARING") {
-            groups.forEach(async group_id => {
-                app.sender.sendGroupMsg(group_id, `${user.nickname}开播了[CQ:at,qq=all]\n${(await axios.request<RoomInfo>(live_request_config)).data.data.room_info.title}\n${room_address}`)
-            })
+            for (let group_id of groups)
+                app.sender.sendGroupMsg(group_id,
+                    `${user.nickname}下播了[CQ:at,qq=all]`
+                    + `\n${(await axios.request<RoomInfo>(live_request_config)).data.data.room_info.title}`
+                    + `\n${room_address} `)
             app.sender.setGroupWholeBan(743492765, false)
         }
     } catch (e) {
@@ -110,7 +117,7 @@ let dynamic_request_config: AxiosRequestConfig = {
 }
 let polling_dynamic = async () => {
     try {
-        (await axios.request<SpaceHistory>(dynamic_request_config))
+        let cards = (await axios.request<SpaceHistory>(dynamic_request_config))
             .data.data.cards
             .filter(v => v.desc.timestamp > last_ts)
             .map<ReOrganizedCard>(v => {
@@ -136,27 +143,43 @@ let polling_dynamic = async () => {
                     throw "未知的orig_type属性"
                 return result
             })
-            .forEach(async v => {
-                if (v.timestamp > last_ts)
-                    last_ts = v.timestamp
-                if (v.category !== undefined) { // 未知
-                    if (v.category === "daily") // 带图片的动态
-                        for (let group_id of groups)
-                            app.sender.sendGroupMsg(group_id, `${v.time}${user.nickname}发布了相簿:\n${v.description}\n${v.pictures!.map(({ img_src }) => `[CQ:image,file=${img_src}]`).join(" ")}\n${v.address}`)
-                } else if (v.origin !== undefined) // 有源的动态
-                    // forEach回调函数中无法正确收窄类型 改用for of
-                    if (v.origin.type === 8) // 转发视频
-                        for (let group_id of groups)
-                            app.sender.sendGroupMsg(group_id, `${v.time}${user.nickname}分享了视频:\n${v.content}\n${v.address}\n[CQ:image,file=${v.origin.cover}]\n${v.origin.address}`)
-                    else if (v.origin.type === 4) // 转发动态
-                        for (let group_id of groups)
-                            app.sender.sendGroupMsg(group_id, `${v.time}${user.nickname}转发了动态:\n${v.content}\n${v.address}\n${v.origin.content}\n${v.origin.address}`)
-                    else
-                        throw "未知的源类型"
-                else // 普通动态
+        for (let card of cards) {
+            if (card.timestamp > last_ts)
+                last_ts = card.timestamp
+            if (card.category !== undefined) { // 未知
+                if (card.category === "daily") // 带图片的动态
                     for (let group_id of groups)
-                        app.sender.sendGroupMsg(group_id, `${v.time}${user.nickname}发布了动态:\n${v.content}\n${v.address}`)
-            })
+                        app.sender.sendGroupMsg(group_id,
+                            `${card.time}${user.nickname}发布了相簿:`
+                            + `\n${card.description}`
+                            + `\n${card.pictures!.map(({ img_src }) => `[CQ:image,file=${img_src}]`).join(" ")}`
+                            + `\n${card.address}`)
+            } else if (card.origin !== undefined) // 有源的动态
+                if (card.origin.type === 8) // 转发视频
+                    for (let group_id of groups)
+                        app.sender.sendGroupMsg(group_id,
+                            `${card.time}${user.nickname}分享了视频:`
+                            + `\n${card.content}`
+                            + `\n${card.address}`
+                            + `\n[CQ:image,file=${card.origin.cover}]`
+                            + `\n${card.origin.address}`)
+                else if (card.origin.type === 4) // 转发动态
+                    for (let group_id of groups)
+                        app.sender.sendGroupMsg(group_id,
+                            `${card.time}${user.nickname}转发了动态:`
+                            + `\n${card.content}`
+                            + `\n${card.address}`
+                            + `\n${card.origin.content}`
+                            + `\n${card.origin.address}`)
+                else
+                    throw "未知的源类型"
+            else // 普通动态
+                for (let group_id of groups)
+                    app.sender.sendGroupMsg(group_id,
+                        `${card.time}${user.nickname}发布了动态:`
+                        + `\n${card.content}`
+                        + `\n${card.address}`)
+        }
     } catch (e) {
         console.error(e)
     }
