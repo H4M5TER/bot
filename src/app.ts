fbbe37eb475cf04d894b4a871199c8dd2621d970
H4M5TER <zhuxx2003@gmail.com>
2020-04-30 04:15:10 +0800

    动态转发重构

--- src/app.ts
+++ src/app.ts
@@ -16,14 +16,7 @@ app.receiver.on("message", async (meta) => {
 app.start()
     .catch((e: Error) => console.error(e))
 
-interface RoomInfo {
-    data: {
-        room_info: {
-            title: string
-        }
-    }
-}
-let room_address = `https://live.bilibili.com/${user.room_id}`, live_request_config: AxiosRequestConfig = {
+let live_request_config: AxiosRequestConfig = {
     url: "https://api.live.bilibili.com/xlive/web-room/v1/index/getInfoByRoom",
     method: "get",
     params: {
@@ -34,6 +27,7 @@ let room = new LiveTCP(user.room_id)
 room.on("live", () => {
     console.log("直播间已连接")
 })
+let room_address = `https://live.bilibili.com/${user.room_id}/`
 room.on("msg", async (data) => {
     try {
         if (data.cmd === "DANMU_MSG") {
@@ -46,7 +40,7 @@ room.on("msg", async (data) => {
             for (let group_id of groups)
                 app.sender.sendGroupMsg(group_id,
                     `${user.nickname}开播了[CQ:at,qq=all]`
-                    + `\n${(await axios.request<RoomInfo>(live_request_config)).data.data.room_info.title}`
+                    + `\n${(await axios.request(live_request_config)).data.data.room_info.title}`
                     + `\n${room_address}`)
             app.sender.setGroupWholeBan(743492765, true)
         }
@@ -54,7 +48,7 @@ room.on("msg", async (data) => {
             for (let group_id of groups)
                 app.sender.sendGroupMsg(group_id,
                     `${user.nickname}下播了[CQ:at,qq=all]`
-                    + `\n${(await axios.request<RoomInfo>(live_request_config)).data.data.room_info.title}`
+                    + `\n${(await axios.request(live_request_config)).data.data.room_info.title}`
                     + `\n${room_address} `)
             app.sender.setGroupWholeBan(743492765, false)
         }
@@ -64,49 +58,7 @@ room.on("msg", async (data) => {
 })
 room.on("error", e => console.error(e))
 
-interface Card {
-    card: string,
-    desc: {
-        timestamp: number
-        dynamic_id_str: string
-        orig_type: number
-        orig_dy_id_str: string
-        origin?: {
-            bvid: string
-        }
-    }
-}
-interface Picture {
-    img_src: string
-}
-interface Item {
-    content?: string
-    category?: string
-    description?: string
-    pictures?: Picture[]
-    timestamp: number
-}
-interface ParsedCard {
-    item: Item
-    origin?: string
-}
-interface Origin {
-    type: number
-    address: string
-    content?: string
-    cover?: string
-}
-interface ReOrganizedCard extends Item {
-    address: string
-    time: string
-    origin?: Origin
-}
-interface SpaceHistory {
-    data: {
-        cards: Card[]
-    }
-}
-let last_ts: number
+let last_ts: number = 0
 let dynamic_request_config: AxiosRequestConfig = {
     url: "https://api.vc.bilibili.com/dynamic_svr/v1/dynamic_svr/space_history",
     method: "get",
@@ -117,77 +69,85 @@ let dynamic_request_config: AxiosRequestConfig = {
 }
 let polling_dynamic = async () => {
     try {
-        let cards = (await axios.request<SpaceHistory>(dynamic_request_config))
-            .data.data.cards
-            .filter(v => v.desc.timestamp > last_ts)
-            .map<ReOrganizedCard>(v => {
-                let card: ParsedCard = JSON.parse(v.card)
-                let result: ReOrganizedCard = {
-                    ...card.item,
-                    time: `[${new Date(v.desc.timestamp * 1000).toLocaleString()}]`,
-                    address: `https://t.bilibili.com/${v.desc.dynamic_id_str}`,
+        let cards = (await axios.request(dynamic_request_config)).data.data.cards
+        let new_cards = cards.filter(({ desc }) => desc.timestamp > last_ts)
+        for (let { desc } of new_cards)
+            if (desc.timestamp > last_ts)
+                last_ts = desc.timestamp
+        let messages = new_cards.map(v => {
+            let desc = v.desc
+            let card = JSON.parse(v.card)
+            let result: any = {
+                type: desc.type,
+                time: new Date(desc.timestamp * 1000).toLocaleString(),
+                address: `https://t.bilibili.com/${desc.dynamic_id_str}/`
+            }
+            if (desc.type === 4) {
+                // 动态
+                result.verb = "发布了动态"
+                result.content = card.item.content
+            } else if (desc.type === 2) {
+                // 相簿
+                result.verb = "发布了相簿"
+                result.content =
+                    `${card.item.description}`
+                    + `\n${card.item.pictures.map(({ img_src }) => `[CQ:image,file=${img_src}]`).join(" ")}`
+            } else if (desc.type === 1) { // 转发
+                let origin = JSON.parse(card.origin)
+                result.origin = {}
+                result.content = card.item.content
+                result.origin.address = `https://t.bilibili.com/${desc.orig_dy_id_str}/`
+                if (desc.orig_type === 8) {
+                    result.verb = "分享了视频"
+                    result.origin.content = `${origin.title}\n[CQ:image,file=${origin.pic}]`
+                    result.origin.address = `https://b23.tv/${desc.origin.bvid}/`
                 }
-                if (v.desc.orig_type === 8)
-                    result.origin = {
-                        type: 8,
-                        address: `https://b23.tv/${v.desc.origin!.bvid}`,
-                        cover: JSON.parse(card.origin!).pic
-                    }
-                else if (v.desc.orig_type === 4)
-                    result.origin = {
-                        type: 4,
-                        address: `https://t.bilibili.com/${v.desc.orig_dy_id_str}`,
-                        content: JSON.parse(card.origin!).item.content
-                    }
-                else if (v.desc.orig_type !== 0)
-                    throw "未知的orig_type属性"
-                return result
-            })
-        for (let card of cards) {
-            if (card.timestamp > last_ts)
-                last_ts = card.timestamp
-            if (card.category !== undefined) { // 未知
-                if (card.category === "daily") // 带图片的动态
-                    for (let group_id of groups)
-                        app.sender.sendGroupMsg(group_id,
-                            `${card.time}${user.nickname}发布了相簿:`
-                            + `\n${card.description}`
-                            + `\n${card.pictures!.map(({ img_src }) => `[CQ:image,file=${img_src}]`).join(" ")}`
-                            + `\n${card.address}`)
-            } else if (card.origin !== undefined) // 有源的动态
-                if (card.origin.type === 8) // 转发视频
-                    for (let group_id of groups)
-                        app.sender.sendGroupMsg(group_id,
-                            `${card.time}${user.nickname}分享了视频:`
-                            + `\n${card.content}`
-                            + `\n${card.address}`
-                            + `\n[CQ:image,file=${card.origin.cover}]`
-                            + `\n${card.origin.address}`)
-                else if (card.origin.type === 4) // 转发动态
-                    for (let group_id of groups)
-                        app.sender.sendGroupMsg(group_id,
-                            `${card.time}${user.nickname}转发了动态:`
-                            + `\n${card.content}`
-                            + `\n${card.address}`
-                            + `\n${card.origin.content}`
-                            + `\n${card.origin.address}`)
-                else
-                    throw "未知的源类型"
-            else // 普通动态
+                else if (desc.orig_type === 4) {
+                    result.verb = "转发了动态"
+                    result.origin.content = origin.item.content
+                }
+                else if (desc.orig_type === 2) {
+                    result.verb = "转发了相簿"
+                    result.origin.content =
+                        `${origin.item.description}`
+                        + `\n${origin.pictures.map(({ img_src }) => `[CQ:image,file=${img_src}]`).join(" ")}`
+                }
+                else if (desc.orig_type !== 0)
+                    throw `未知的orig_type字段值:${desc.orig_type}`
+            } else if (desc.type === 8) {
+                // 视频
+                result.verb = "发布了视频"
+                result.content = card.dynamic
+                result.address = `https://b23.tv/${desc.bvid}/`
+            }
+            return result
+        })
+        for (let message of messages)
+            if (message.type !== 1)
                 for (let group_id of groups)
                     app.sender.sendGroupMsg(group_id,
-                        `${card.time}${user.nickname}发布了动态:`
-                        + `\n${card.content}`
-                        + `\n${card.address}`)
-        }
+                        `[${message.time}]${user.nickname}${message.verb}`
+                        + `\n${message.content}`
+                        + `\n${message.address}`
+                    )
+            else
+                for (let group_id of groups)
+                    app.sender.sendGroupMsg(group_id,
+                        `[${message.time}]${user.nickname}${message.verb}`
+                        + `\n${message.content}`
+                        + `\n${message.address}`
+                        + `\n\n${message.origin.content}`
+                        + `\n${message.origin.address}`
+                    )
     } catch (e) {
         console.error(e)
     }
     setTimeout(polling_dynamic, config.delay)
 }
-axios.request<SpaceHistory>(dynamic_request_config).then(resp => {
-    if (last_ts = resp.data.data.cards[0].desc.timestamp) {
-        // 任何falsy value都throw 包括 0 undefined null
+
+axios.request(dynamic_request_config).then(resp => {
+    if (resp?.data?.data?.cards[0]?.desc?.timestamp) {
+        last_ts = resp.data.data.cards[0].desc.timestamp
         setTimeout(polling_dynamic, config.delay)
         console.log("动态已连接")
     }
