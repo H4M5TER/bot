9109b52d68202136ce7a36f825fb492e891973c0
H4M5TER <zhuxx2003@gmail.com>
2021-05-17 08:46:24 +0800

    chore

--- init.sh
+++ init.sh
@@ -4,10 +4,10 @@ name=$(curl https://api.github.com/repos/mrs4s/go-cqhttp/releases/latest | grep 
 curl https://api.github.com/repos/mrs4s/go-cqhttp/releases/latest | grep -Eo 'https.+amd64.deb' | xargs wget
 sudo apt install ./$name -y
 rm $name
-if [ ! -d "~/.go-cqhttp" ]; then
+if [ ! -d "$HOME/.go-cqhttp" ]; then
 mkdir ~/.go-cqhttp
 cd ~/.go-cqhttp
 go-cqhttp
+read -p "编辑完配置文件后继续" null
 fi
-read -p "编辑完配置文件后继续" name
 npx pm2 start go-cqhttp --cwd ~/.go-cqhttp --no-daemon
